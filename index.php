27ff364178b0e81231a4fe749e0b1f391d0e692c
Christhian Aracena <christhian.aracena@correoaiep.cl>
2023-10-30 12:57:46 -0300

    avance front index

--- index.php
+++ index.php
@@ -1,13 +1,103 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="Presentacion/index.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Freehand&family=Roboto:wght@300&display=swap" rel="stylesheet">
+
+
+    <link href="https://fonts.googleapis.com/css2?family=Freehand&family=Raleway&family=Roboto:wght@300&display=swap" rel="stylesheet">
     <title>Document</title>
 </head>
+
 <body>
-    <header>hola</header>
-    
+    <header class="header-landing flex-row flex-wrap">
+        <div class="contenedor-header contenedor">
+
+            <p class="title sombra">Rescatamigos</p>
+            <img class="logo" src="img/logo1_v2-removebg-preview.png" alt="" srcset="">
+
+        </div>
+
+        <div class="contenedor flex-row ">
+            <p class="counts cursor-pointer">Crea tu cuenta</p>
+            <p class="last cursor-pointer">Iniciar sesion</p>
+        </div>
+
+    </header>
+
+    <main class="contenedor">
+
+        <h3 class="fouroptions">Tienes 4 opciones para la gestión de mascotas:</h3>
+
+        <div class="container-options flex-wrap">
+
+            <div class="adopt">
+                <p>a</p>
+            </div>
+
+            <div class="inAdopt">
+                <p>a</p>
+            </div>
+
+            <div class="foundIt">
+                <p>a</p>
+            </div>
+
+            <div class="seeIt">
+                <p>a</p>
+            </div>
+
+        </div>
+
+
+
+    </main>
+    <div class="container-stadistics fouroptions">
+        <h3>Estadisticas</h3>
+    </div>
+
+
+    <footer class="footer">
+        <div class="footer-container contenedor">
+            <div class="content1 sombra">
+                <h3>Informate</h3>
+                <p>Reglamento</p>
+                <p>Preguntas frecuentes</p>
+                <p>Quienes Somos</p>
+                <p>Donaciones</p>
+            </div>
+
+            <div class="content1 sombra">
+                <h3>Mantente conectado</h3>
+                <p>Facebook</p>
+                <p>Instagram</p>
+                <p>Twitter</p>
+
+            </div>
+            <div class="content2 sombra">
+                <h3>Dejanos un mensaje.</h3>
+                <input type="text" placeholder="Nombre">
+                <input type="text" placeholder="Correo">
+                <div>
+                    <input type="text" placeholder="Asunto">
+                </div>
+                <div class="text">
+                    <textarea placeholder="Tu mensaje..."></textarea>
+                    <button>Enviar</button>
+                </div>
+
+            </div>
+
+        </div>
+
+        <small class="contenedor powered sombra">© 2023 Powered By Christhian Aracena.</small>
+    </footer>
+
 </body>
+
 </html>
