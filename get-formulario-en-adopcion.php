41d967b280352f2498a1b73a29d7d2c6160f58be
Christhian Aracena <christhian.aracena@correoaiep.cl>
2023-12-03 08:49:29 -0300

    backend

--- get-formulario-en-adopcion.php
+++ get-formulario-en-adopcion.php
@@ -1,5 +1,5 @@
 <div class="container-register container-options">
-    <h2>Rellena los campos</h2>
+    <h2>En adopción</h2>
     <form action="#" method="post" enctype="multipart/form-data">
         <div class="form-group">
             <input type="text" name="nombre" required placeholder="Nombre de mascota">
@@ -26,12 +26,12 @@
             <?php include("Negocio/get-combox-edad.php"); ?>
         </div>
         <div class="form-group">
-            <textarea type="text" name="vacunas" required placeholder="Incluye todos los detalles relevantes como requisitos de adopcion y descripcion completa acerca de la personalidad de la mascota."></textarea>
+            <textarea type="text" name="vacunas" required placeholder="Incluye la mayor cantidad de detalles."></textarea>
         </div>
         <div class="form-group">
             <label for="avatar">Foto de Perfil (opcional):</label>
             <input type="file" id="avatar" name="avatar" accept="image/*">
         </div>
-        <button id="btnRegister">Registrarse</button>
+        <button id="btnRegister">Dar en adopción</button>
     </form>
 </div>
